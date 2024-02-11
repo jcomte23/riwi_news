40620e519933843be028def9ca5d65ba9344fb1a
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-02-11 11:52:24 -0500

    terminacion en la funcion de mostrar news

---
+++ src/js/news.js
@@ -0,0 +1,40 @@
+import '../scss/style.scss'
+import * as bootstrap from 'bootstrap'
+
+const containerNews = document.getElementById("container-news")
+
+const indexNews = (data) => {
+    data.forEach(element => {
+        containerNews.innerHTML += `
+        <section class="card mb-3" style="max-width: 540px">
+            <article class="row g-0">
+            <div class="col-md-4">
+                <img src="${element.image}" class="img-fluid rounded-start" alt="image news" />
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                <h5 class="card-title">${element.title}</h5>
+                <p class="card-text">
+                    ${element.content}
+                </p>
+                <p class="card-text">
+                    <small class="text-body-secondary">${element.publicationDate}</small>
+                </p>
+                </div>
+            </div>
+            </article>
+        </section>
+        `
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    getNews()
+})
+
+
+async function getNews() {
+    const response = await fetch("http://localhost:3000/news")
+    const data = await response.json()
+    indexNews(data)
+}
