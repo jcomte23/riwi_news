952eaf581ea30dcf0a21769dd46c5ae7b0c9dba4
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-02-11 12:49:11 -0500

    implementacion del login

--- src/js/users.js
+++ src/js/users.js
@@ -1,2 +1,29 @@
 import '../scss/style.scss'
 import * as bootstrap from 'bootstrap'
+
+const formLogin = document.getElementById("form-login")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+
+formLogin.addEventListener("submit", async (event) => {
+    event.preventDefault()
+    const user=await showUser(email.value)
+    if (user===undefined) {
+        alert("el email no esta registrado")
+    }else{
+        if (user.password === password.value) {
+            localStorage.setItem("userOnline",JSON.stringify(user))
+            window.location.href="../admin/administrator.html"
+        }else{
+            alert("login incorrecto")
+        }
+    }
+})
+
+async function showUser(email) {
+    const response = await fetch(`http://localhost:3000/users?email=${email}`)
+    const data = await response.json()
+    if (data.length === 1) {
+        return data[0]
+    }
+}
